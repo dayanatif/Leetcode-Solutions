8ef47bc80bb6c66f1f63f4dca4452c699b63cac9
Rao Muhammad Dayan Atif <162185730+dayanatif@users.noreply.github.com>
2025-01-02 03:57:56 +0500

    Create 1512. Number of Good Pairs.py

---
+++ 1512. Number of Good Pairs.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        pairs = 0
+
+        for i in range(len(nums)):
+            for j in range(i):
+                if nums[i]==nums[j]:
+                    pairs+=1
+        return pairs
+        
