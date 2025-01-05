42c879075a144d25d9cbfd8e43c7d711b72b4e13
Rao Muhammad Dayan Atif <162185730+dayanatif@users.noreply.github.com>
2025-01-06 03:31:09 +0500

    Create 1920. Build Array from Permutation.py

---
+++ 1920. Build Array from Permutation.py
@@ -0,0 +1,8 @@
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        ans = []
+        for i in range (len(nums)):
+            x = nums[nums[i]]
+            ans.append(x)
+        return ans
+        
